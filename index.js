874997eb3f05586e34d957fb62c32f1cdbcb6605
Kazushi Konosu <kazushi@konosu.me>
2020-01-09 02:27:32 +0900

    create config

---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const colors = require('./colors')
+
+exports.decorateConfig = config => {
+	const themeConfig = config.horizonTheme || {}
+
+	const selectedTheme =
+		themeConfig.theme === 'bright' ? 'bright' : 'normal'
+
+	const {
+		tabColor,
+		backgroundColor,
+		foregroundColor,
+		borderColor,
+		cursorColor,
+		selectionColor,
+		lightBlue,
+		lightCyan,
+		lightGreen,
+		lightMagenta,
+		lightRed,
+		lightYellow,
+		black,
+		blue,
+		cyan,
+		green,
+		magenta,
+		red,
+		yellow,
+		white
+	} = colors[selectedTheme]
+
+	return {
+		...config,
+		backgroundColor,
+		foregroundColor,
+		borderColor,
+		cursorColor,
+		cursorAccentColor: backgroundColor,
+		selectionColor,
+		colors: {
+			lightBlue,
+			lightCyan,
+			lightGreen,
+			lightMagenta,
+			lightRed,
+			lightYellow,
+			black,
+			blue,
+			cyan,
+			green,
+			magenta,
+			red,
+			yellow,
+			white
+		},
+		css: `
+			.tab_tab.tab_active::before {
+				content: '';
+				position: absolute;
+				bottom: 0;
+				left: 0;
+				right: 0;
+				height: 1px;
+				background-color: ${tabColor};
+			}
+
+			.tab_text,
+			.term_term {
+				opacity: 0.6;
+			}
+
+			.tab_active .tab_text,
+			.term_active .term_term {
+				opacity: 1;
+			}
+
+			${config.css || ''}
+		`
+	}
+}

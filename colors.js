a09123bd20274ceceb5ab49b63edc87a5a3c1ad1
Kazushi Konosu <kazushi@konosu.me>
2020-01-09 01:26:50 +0900

    update colors

--- colors.js
+++ colors.js
@@ -1,35 +1,45 @@
 const normal = {
 	backgroundColor: '#1C1E26',
 	foregroundColor: '#D5D8DA',
+	borderColor: '#1A1C23',
+	cursorColor: 'rgba(46, 48, 62, 0.8)',
+	selectionColor: 'rgba(46, 48, 62, 0.5)',
 	lightBlue: '#26BBD9',
 	lightCyan: '#59E1E3',
 	lightGreen: '#29D398',
 	lightMagenta: '#EE64AC',
 	lightRed: '#E95678',
 	lightYellow: '#FAB795',
+	black: '#06060C',
 	blue: '#3FC4DE',
 	cyan: '#6BE4E6',
 	green: '#3FDAA4',
 	magenta: '#F075B5',
 	red: '#EC6A88',
-	yellow: '#FBC3A7'
+	yellow: '#FBC3A7',
+	white: '#D5D8DA',
 }
 
 const bright = {
 	backgroundColor: '#FDF0ED',
 	foregroundColor: '#06060C',
+	borderColor: '#1A1C23',
+	cursorColor: 'rgba(249, 203, 190, 0.8)',
+	selectionColor: 'rgba(249, 203, 190, 0.5)',
 	lightBlue: '#26BBD9',
 	lightCyan: '#59E1E3',
 	lightGreen: '#29D398',
 	lightMagenta: '#EE64AC',
 	lightRed: '#E95678',
 	lightYellow: '#FAB795',
+	black: '#06060C',
 	blue: '#3FC4DE',
 	cyan: '#6BE4E6',
 	green: '#3FDAA4',
 	magenta: '#F075B5',
 	red: '#EC6A88',
-	yellow: '#FBC3A7'
+	yellow: '#FBC3A7',
+	white: '#D5D8DA'
 }
 
 module.exports = {

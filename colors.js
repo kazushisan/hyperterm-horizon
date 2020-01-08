918312423b591457ca60c2d43c3db58d30173e50
Kazushi Konosu <kazushi@konosu.me>
2020-01-09 00:59:49 +0900

    add colors

---
+++ colors.js
@@ -0,0 +1,38 @@
+const normal = {
+	backgroundColor: '#1C1E26',
+	foregroundColor: '#D5D8DA',
+	lightBlue: '#26BBD9',
+	lightCyan: '#59E1E3',
+	lightGreen: '#29D398',
+	lightMagenta: '#EE64AC',
+	lightRed: '#E95678',
+	lightYellow: '#FAB795',
+	blue: '#3FC4DE',
+	cyan: '#6BE4E6',
+	green: '#3FDAA4',
+	magenta: '#F075B5',
+	red: '#EC6A88',
+	yellow: '#FBC3A7'
+}
+
+const bright = {
+	backgroundColor: '#FDF0ED',
+	foregroundColor: '#06060C',
+	lightBlue: '#26BBD9',
+	lightCyan: '#59E1E3',
+	lightGreen: '#29D398',
+	lightMagenta: '#EE64AC',
+	lightRed: '#E95678',
+	lightYellow: '#FAB795',
+	blue: '#3FC4DE',
+	cyan: '#6BE4E6',
+	green: '#3FDAA4',
+	magenta: '#F075B5',
+	red: '#EC6A88',
+	yellow: '#FBC3A7'
+}
+
+module.exports = {
+	normal,
+	bright
+}
